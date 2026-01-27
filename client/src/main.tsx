87f15e6ccd259ba9039c95bc5ed17a7a40d62f14
Quinlan Turner <quinlanturner@Quinlans-MacBook-Pro.local>
2026-01-26 17:44:54 -0700

    Cache Bust: Force fresh CSS load

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 console.log("[DEBUG] main.tsx entry point reached");
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css?v=noir";
 
 createRoot(document.getElementById("root")!).render(<App />);
