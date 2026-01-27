3d50f65e61a2f7c4dc4e523ca18b4ad1276a240d
Quinlan Turner <quinlanturner@Quinlans-MacBook-Pro.local>
2026-01-26 18:03:19 -0700

    UI Enhancement: Add Depth (Spotlight + Glassmorphism)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 console.log("[DEBUG] main.tsx entry point reached");
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css?v=noir2";
+import "./index.css?v=noir_depth";
 
 createRoot(document.getElementById("root")!).render(<App />);
